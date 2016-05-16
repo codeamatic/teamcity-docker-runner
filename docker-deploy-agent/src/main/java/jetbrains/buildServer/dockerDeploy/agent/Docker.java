40b6f5cc4bce21a181bdc2e25f93b5df38365684
Jason Webb <jason@jswebb.net>
2016-05-16 01:20:22 -0400

    Added code for detecting docker version through an Agent

--- docker-deploy-agent/src/main/java/jetbrains/buildServer/dockerDeploy/agent/Docker.java
+++ docker-deploy-agent/src/main/java/jetbrains/buildServer/dockerDeploy/agent/Docker.java
@@ -1,19 +1,73 @@
 package jetbrains.buildServer.dockerDeploy.agent;
 
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.text.StringUtil;
+
 import org.apache.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import jetbrains.buildServer.ExecResult;
+import jetbrains.buildServer.SimpleCommandLineProcessRunner;
 
 public class Docker {
   private static final Logger LOG = Logger.getLogger(Docker.class);
 
   /**
    * Determines whether or not Docker is available on the current system or not.
-   * Somewhat based on the DockerVM class programmed by Jonnyzzz for Teamcity.virtual.
+   * Heavily based on the DockerVM class programmed by Jonnyzzz for Teamcity.virtual.
    *
-   * @return true if Docker is available, false otherwise
+   * @return version of Docker installed, null if Docker doesn't exist
    */
+  @Nullable
   public String getDockerVersion() {
+    final String output = executeCommandWithShell("docker --version");
+    if (output == null) {
+      return null;
+    }
 
-    return "1.13.0";
+    String ver = output.toLowerCase().trim();
+    ver = ver.replaceAll("\\s*docker\\s+version\\s+", "");
+    ver = ver.replaceAll(",?\\s+build\\s+", "-");
+
+    if (StringUtil.isEmptyOrSpaces(ver)) {
+      LOG.warn("Failed to parse docker version: " + output);
+      return null;
+    }
+
+    return ver;
   }
 
+  /**
+   * Helper method for executing shell commands directly within the Agent.
+   * Heavily based on the DockerVM class programmed by Jonnyzzz for Teamcity.virtual.
+   *
+   * @param command Command line command to be run on the agent
+   * @return String result of the command being run.
+   */
+  @Nullable
+  private String executeCommandWithShell(@NotNull final String... command) {
+    GeneralCommandLine cmd = new GeneralCommandLine();
+    if (SystemInfo.isWindows) {
+      cmd.setExePath("cmd");
+      cmd.addParameters("/c");
+    } else {
+      cmd.setExePath("/bin/sh");
+      cmd.addParameters("-c");
+    }
+    cmd.addParameters(command);
+
+    LOG.info("Running: " + cmd.getCommandLineString());
+
+    final ExecResult result = SimpleCommandLineProcessRunner.runCommand(cmd, new byte[0]);
+
+    //noinspection ThrowableResultOfMethodCallIgnored
+    if (result.getException() != null || result.getExitCode() != 0) {
+      LOG.info(("Failed running command [" + cmd.getCommandLineString() + "]. Exit code: " + result.getExitCode() + "\n " + result.getStdout() + "\n" + result.getStderr()).trim());
+      return null;
+    }
+
+    return result.getStdout();
+  }
 }
