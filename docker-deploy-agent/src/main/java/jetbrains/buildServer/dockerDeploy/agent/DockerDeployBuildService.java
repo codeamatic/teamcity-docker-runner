e6cde24b73e10279c0afa7a7882dfd952b62fe01
Jason Webb <jason.webb@rockfishinteractive.com>
2017-04-18 11:00:55 -0400

    Added dockerfile and dockerignore file generators with hardcoded content

--- docker-deploy-agent/src/main/java/jetbrains/buildServer/dockerDeploy/agent/DockerDeployBuildService.java
+++ docker-deploy-agent/src/main/java/jetbrains/buildServer/dockerDeploy/agent/DockerDeployBuildService.java
@@ -3,6 +3,11 @@ package jetbrains.buildServer.dockerDeploy.agent;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -12,13 +17,11 @@ import jetbrains.buildServer.agent.plugins.beans.PluginDescriptor;
 import jetbrains.buildServer.agent.runner.BuildServiceAdapter;
 import jetbrains.buildServer.agent.runner.ProgramCommandLine;
 import jetbrains.buildServer.agent.runner.SimpleProgramCommandLine;
-import jetbrains.buildServer.dockerDeploy.common.DockerDeployBuildFromType;
 import jetbrains.buildServer.dockerDeploy.common.DockerDeployConstants;
-import jetbrains.buildServer.util.OSType;
-import jetbrains.buildServer.util.StringUtil;
 
 public class DockerDeployBuildService extends BuildServiceAdapter {
   private static final Logger LOG = Logger.getLogger(DockerDeployBuildService.class.getName());
+  private static final String DOCKERFILE_ARG_NAME = "PUBDIR";
 
   private final PluginDescriptor pluginDescriptor;
 
@@ -30,6 +33,7 @@ public class DockerDeployBuildService extends BuildServiceAdapter {
   @Override
   public ProgramCommandLine makeProgramCommandLine() throws RunBuildException {
 
+    String checkoutDir = getRunnerParameters().get("teamcity.build.checkoutDir");
     List<String> params = new LinkedList<String>();
     String exeName = DockerDeployConstants.RUNNER_EXE_NAME;
 
@@ -37,19 +41,56 @@ public class DockerDeployBuildService extends BuildServiceAdapter {
     Map<String, String> runnerParameters = getRunnerParameters();
     String dockerDeployType = runnerParameters.get(DockerDeployConstants.SETTINGS_BUILD_FROM);
 
-    // Deploying from custom Dockerfile associated with project
-    if(ConfigurationParamsUtil.isDockerfileBuildType(dockerDeployType)) {
-      String location = runnerParameters.get(DockerDeployConstants.SETTINGS_DOCKERFILE_LOCATION);
-      params.add("run");
-    } else if(ConfigurationParamsUtil.isDockerComposeBuildType(dockerDeployType)) {
-      // Change executable name to docker-compose
-      exeName = DockerDeployConstants.RUNNER_COMPOSE_EXE_NAME;
-      params.add("--version");
-    } else {
-      //
-      params.add("--xyz");
-    }
+    generateDockerfile(checkoutDir);
+    generateDockerIgnore(checkoutDir);
 
     return new SimpleProgramCommandLine(getRunnerContext(), exeName, params);
+  }
+
+  /**
+   * Generates a default Dockerfile to be used with projects that don't
+   * include their own Dockerfile.
+   *
+   * @param checkoutDirectory Directory where all resources have been checked out
+   * @throws RunBuildException Thrown when unable to write Dockerfile
+   */
+  private void generateDockerfile(String checkoutDirectory) throws RunBuildException {
+    Path filePath = Paths.get(checkoutDirectory + "\\Dockerfile");
+
+    if(filePath.toFile().exists()) {
+      getLogger().message("Overwriting existing Dockerfile with a generic default.");
+    }
+
+    try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {
+      String lineSeparator = System.getProperty("line.separator");
+      writer.write("FROM alpine:3.4" + lineSeparator);
+      writer.write("ARG " + DOCKERFILE_ARG_NAME + lineSeparator);
+      writer.write("COPY [\"$" + DOCKERFILE_ARG_NAME + "\", \"/var/www/html\"]");
+    } catch(IOException ex) {
+      throw new RunBuildException("Unable to write Dockerfile: " + ex.getMessage());
+    }
+  }
+
+
+  /**
+   * Generates a default .dockerignore file.  Ignoring node_modules and bower_components.
+   *
+   * @param checkoutDirectory Directory where all resources have been checked out
+   * @throws RunBuildException Thrown when unable to write .dockerignore
+   */
+  private void generateDockerIgnore(String checkoutDirectory) throws RunBuildException {
+    Path filePath = Paths.get(checkoutDirectory + "\\.dockerignore");
+
+    if(filePath.toFile().exists()) {
+      getLogger().message("Overwriting existing .dockerignore file with a generic default.");
+    }
+
+    try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {
+      String lineSeparator = System.getProperty("line.separator");
+      writer.write("node_modules" + lineSeparator);
+      writer.write("bower_components");
+    } catch(IOException ex) {
+      throw new RunBuildException("Unable to write .dockerignore file: " + ex.getMessage());
+    }
   }
 }
